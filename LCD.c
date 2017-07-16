1374c0a8237693f8ee96a8e19f2821b2ffb1b826
Mohamed Yousef <engmohamedyussef@gmail.com>
2017-07-16 13:33:42 +0200

    Add files via upload

---
+++ LCD.c
@@ -0,0 +1,59 @@
+/*
+ * LCD.c
+ *
+ * Created: 6/14/2017 7:47:36 PM
+ *  Author: MOHAMED YOUSEF
+ */ 
+
+#include "LCD.h"
+
+void LCD_init(void){
+	LCD_data_ddr = 0xFF;
+	LCD_data_port = 0x00;
+	
+	LCD_con_ddr = 0xFF;
+	LCD_con_port = 0x00;
+	_delay_ms(2);
+	LCD_send_command(0x38);
+	LCD_send_command(0x0C);
+	_delay_ms(2);
+}
+
+void LCD_send_command(byte command){
+	LCD_data_port = command;
+	
+	CLR_BIT(LCD_con_port, 0); //rs = 0
+	CLR_BIT(LCD_con_port, 1); //rw = 0
+	
+	//enable - high and low
+	CLR_BIT(LCD_con_port, 2);
+	_delay_us(1000);
+	SET_BIT(LCD_con_port, 2);
+	
+	_delay_ms(5);
+}
+
+void LCD_send_data(byte data){
+	LCD_data_port = data;
+	
+	SET_BIT(LCD_con_port, 0); //rs = 1
+	CLR_BIT(LCD_con_port, 1); //rw = 0
+	
+	//enable - high and low
+	CLR_BIT(LCD_con_port, 2);
+	_delay_us(1);
+	SET_BIT(LCD_con_port, 2);
+	
+	_delay_ms(5);
+}
+
+void LCD_print_string(byte *string){
+	while (*string != '\0'){
+		LCD_send_data(*string);
+		string++;
+	}	
+}
+
+void LCD_clear(void){
+	LCD_send_command(0x02);
+}
---
+++ LCD.h
@@ -0,0 +1,40 @@
+/*
+ * LCD.h
+ *
+ * Created: 6/14/2017 7:47:26 PM
+ *  Author: MOHAMED YOUSEF
+ */ 
+
+#ifndef LCD_H_
+#define LCD_H_
+
+#include <avr/io.h>
+#include <util/delay.h>
+
+typedef unsigned char byte;
+
+#define SET_BIT(reg, index) reg |= (1 << index)
+#define CLR_BIT(reg, index)	reg &= (~(1 << index))
+#define GET_BIT(reg, index)((reg >> index) & 0x01)
+#define TOG_BIT(reg, index)	reg ^= (1 << index)
+
+#define LCD_data_ddr  DDRA
+#define LCD_data_port PORTA
+#define LCD_data_pin  PINA
+
+#define LCD_con_ddr  DDRB	
+#define LCD_con_port PORTB
+#define LCD_con_pin  PINB
+
+#define LCD_RS  0
+#define LCD_RW 1
+#define LCD_enable  1
+
+void LCD_init(void);
+void LCD_send_command(byte command);
+void LCD_send_data(byte data);
+void LCD_print_string(byte *string);
+void LCD_clear(void);
+
+
+#endif /* LCD_H_ */
